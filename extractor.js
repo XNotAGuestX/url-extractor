8c1ed24e10336021f6290331d05c1e49f9e7cd8a
XNotAGuestX <98876928+XNotAGuestX@users.noreply.github.com>
2023-09-12 19:52:43 +0200

    bugfix second url

--- extractor.js
+++ extractor.js
@@ -5,24 +5,31 @@ const rl = readline.createInterface({
 	output: process.stdout,
 });
 strikes = 0;
+console.clear();
 startExtract();
 
 function startExtract() {
 	new Promise((resolve, reject) => {
 		rl.question('Enter HTML Embed\n', (inputEmbed) => {
 			let httpsPos = inputEmbed.indexOf('https://');
-			if (httpsPos > 0) {
-				console.log(
-					inputEmbed
-						.substring(
-							httpsPos,
-							inputEmbed.substring(httpsPos).indexOf('"') + httpsPos,
-						)
-						.replace('100', '500'),
-				);
+			if (httpsPos > -1) {
+				console.clear();
+				if (inputEmbed.includes('"')) {
+					console.log(
+						inputEmbed
+							.substring(
+								httpsPos,
+								inputEmbed.substring(httpsPos).indexOf('"') + httpsPos,
+							)
+							.replace('/100/', '/800/'),
+					);
+				} else {
+					console.log(inputEmbed.replace('?size=1x', '?size=4x'));
+				}
 				strikes = 0;
 			} else {
 				strikes++;
+				console.log(httpsPos);
 				console.log('Strike ' + strikes);
 				if (strikes >= 2) reject('bruh');
 			}
